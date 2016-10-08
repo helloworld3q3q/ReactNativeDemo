1a4bd93cb696f2fe0b7570e68b6c52637944d175
helloworld3q3q <helloworld3q3q@gmail.com>
2016-10-08 15:43:55 +0800

    node_modules改动后的插件
    node_modules 里需要用的插件 根据自己需求进行改动

---
+++ modules/react-native-swipe-cards/README.md
@@ -0,0 +1,248 @@
+# Tinder Cards for React Native
+
+LOOKING FOR CONTRIBUTORS. I'm not currently using this in any active projects. If anyone wants to be added, please write me.
+
+A [package](https://www.npmjs.com/package/react-native-swipe-cards) based on [@brentvatne](https://github.com/brentvatne/)'s awesome [example](https://github.com/brentvatne/react-native-animated-demo-tinder), based in turn on the Tinder swipe interface.
+
+
+![react native tinder cards](https://raw.githubusercontent.com/meteor-factory/react-native-tinder-swipe-cards/master/screenshots/react-native-tinder-cards.gif)
+
+![react native tinder cards pugs](https://raw.githubusercontent.com/meteor-factory/react-native-tinder-swipe-cards/master/screenshots/react-native-tinder-cards-pugs.gif)
+
+## Quick Start
+1. `npm install --save react-native-swipe-cards`
+2. Create a module e.g. `Tinder.js`
+3. Import it `import Tinder from './Tinder.js'`
+4. Render it `<Tinder style={{flex: 1}} />`
+
+```javascript
+// Tinder.js
+'use strict';
+
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+import SwipeCards from 'react-native-swipe-cards';
+
+let Card = React.createClass({
+  render() {
+    return (
+      <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+        <Text>{this.props.text}</Text>
+      </View>
+    )
+  }
+})
+
+const Cards = [
+  {text: 'Tomato', backgroundColor: 'red'},
+  {text: 'Aubergine', backgroundColor: 'purple'},
+  {text: 'Courgette', backgroundColor: 'green'},
+  {text: 'Blueberry', backgroundColor: 'blue'},
+  {text: 'Umm...', backgroundColor: 'cyan'},
+  {text: 'orange', backgroundColor: 'orange'},
+]
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      cards: Cards
+    }
+  },
+  handleYup (card) {
+    console.log(`Yup for ${card.text}`)
+  },
+  handleNope (card) {
+    console.log(`Nope for ${card.text}`)
+  },
+  render() {
+    return (
+      <SwipeCards
+        cards={this.state.cards}
+
+        renderCard={(cardData) => <Card {...cardData} />}
+        renderNoMoreCards={() => <NoMoreCards />}
+
+        handleYup={this.handleYup}
+        handleNope={this.handleNope}
+      />
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+  }
+})
+
+```
+
+### More complex example
+```javascript
+'use strict';
+
+import React, {StyleSheet, Text, View, Image} from 'react-native';
+
+import SwipeCards from 'react-native-swipe-cards';
+
+let Card = React.createClass({
+  render() {
+    return (
+      <View style={styles.card}>
+        <Image style={styles.thumbnail} source={{uri: this.props.image}} />
+        <Text style={styles.text}>This is card {this.props.name}</Text>
+      </View>
+    )
+  }
+})
+
+let NoMoreCards = React.createClass({
+  render() {
+    return (
+      <View style={styles.noMoreCards}>
+        <Text>No more cards</Text>
+      </View>
+    )
+  }
+})
+
+const Cards = [
+  {name: '1', image: 'https://media.giphy.com/media/GfXFVHUzjlbOg/giphy.gif'},
+  {name: '2', image: 'https://media.giphy.com/media/irTuv1L1T34TC/giphy.gif'},
+  {name: '3', image: 'https://media.giphy.com/media/LkLL0HJerdXMI/giphy.gif'},
+  {name: '4', image: 'https://media.giphy.com/media/fFBmUMzFL5zRS/giphy.gif'},
+  {name: '5', image: 'https://media.giphy.com/media/oDLDbBgf0dkis/giphy.gif'},
+  {name: '6', image: 'https://media.giphy.com/media/7r4g8V2UkBUcw/giphy.gif'},
+  {name: '7', image: 'https://media.giphy.com/media/K6Q7ZCdLy8pCE/giphy.gif'},
+  {name: '8', image: 'https://media.giphy.com/media/hEwST9KM0UGti/giphy.gif'},
+  {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
+]
+
+const Cards2 = [
+  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
+  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
+  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
+  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
+]
+
+export default React.createClass({
+  getInitialState() {
+    return {
+      cards: Cards,
+      outOfCards: false
+    }
+  },
+  handleYup (card) {
+    console.log("yup")
+  },
+  handleNope (card) {
+    console.log("nope")
+  },
+  cardRemoved (index) {
+    console.log(`The index is ${index}`);
+
+    let CARD_REFRESH_LIMIT = 3
+
+    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
+      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
+
+      if (!this.state.outOfCards) {
+        console.log(`Adding ${Cards2.length} more cards`)
+
+        this.setState({
+          cards: this.state.cards.concat(Cards2),
+          outOfCards: true
+        })
+      }
+
+    }
+
+  },
+  render() {
+    return (
+      <SwipeCards
+        cards={this.state.cards}
+        loop={false}
+
+        renderCard={(cardData) => <Card {...cardData} />}
+        renderNoMoreCards={() => <NoMoreCards />}
+        showYup={true}
+        showNope={true}
+
+        handleYup={this.handleYup}
+        handleNope={this.handleNope}
+        cardRemoved={this.cardRemoved}
+      />
+    )
+  }
+})
+
+const styles = StyleSheet.create({
+  card: {
+    alignItems: 'center',
+    borderRadius: 5,
+    overflow: 'hidden',
+    borderColor: 'grey',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    elevation: 1,
+  },
+  thumbnail: {
+    flex: 1,
+    width: 300,
+    height: 300,
+  },
+  text: {
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  noMoreCards: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
+```
+
+### Props
+| Props name        | Type     | Description                                          | Default |
+|-------------------|----------|------------------------------------------------------|---------|
+| cards*            | Array    | Data that will be provided as props for the cards    |         |
+| renderCard*       | Function | Renders the card with the current data               |          |
+| loop              | Boolean  | If true, start again when run out of cards           | `false` |
+| renderNoMoreCards | Function | Renders what is shown after swiped last card         |         |
+| showYup           | Boolean  | Shows the 'Yup' component                            | `true`  |
+| showNope          | Boolean  | Shows the 'Nope'                                     | `true`  |
+| renderYup         | Function | Renders Yup                                          | `true`  |
+| renderYup         | Function | Renders Nope                                          | `true`  |
+| handleYup         | Function | Called when card is 'passed' with that card's data   |         |
+| handleNope        | Function | Called when card is 'rejected' with that card's data |         |
+| containerStyle    | style | Override default style                                  |         |
+| yupStyle          | style | Override default style                                  |         |
+| yupTextStyle      | style | Override default style                                  |         |
+| nopeStyle         | style | Override default style                                  |         |
+| nopeTextStyle     | style | Override default style                                  |         |
+| yupView           | element | React component to render on a Yes vote                                 |         |
+| yupText           | string  | Text to render on Yes vote                                  |         |
+| noView           | element | React component to render on a No vote                                 |         |
+| noText           | string  | Text to render on No vote                                  |         |
+
+*required
+
+### Todo (PRs welcome!)
+- [ ] Show next card underneath current card
+- [ ] Shadow when card is being dragged
+- [ ] Example with backend
+- [ ] Example with polaroids
+- [ ] Submit to repos
+- [x] renderYup
+- [x] renderNope
+- [ ] Testing
+- [ ] Add more args to `cardRemoved`?
+- [ ] `class extends` all components
---
+++ modules/react-native-swipe-cards/SwipeCards.js
@@ -0,0 +1,354 @@
+/* Gratefully copied from https://github.com/brentvatne/react-native-animated-demo-tinder */
+'use strict';
+
+import React, {Component} from 'react';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Animated,
+    PanResponder,
+    Image,
+    AsyncStorage,
+} from 'react-native';
+
+import clamp from 'clamp';
+
+import Defaults from './Defaults.js';
+
+var SWIPE_THRESHOLD = 100;
+
+// Base Styles. Use props to override these values
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    yup: {
+        position: 'absolute',
+        padding: 20,
+        bottom: 20,
+        borderRadius: 5,
+        right: 20,
+    },
+    yupText: {
+        fontSize: 16,
+        color: 'green',
+    },
+    nope: {
+        position: 'absolute',
+        bottom: 20,
+        padding: 20,
+        left: 20,
+    },
+    nopeText: {
+        fontSize: 16,
+        color: 'red',
+    }
+});
+
+class SwipeCards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+      card: this.props.cards ? this.props.cards[0] : null,
+      direction: true, // next true， prev false
+    }
+  }
+
+  _goToNextCard(press) {
+
+    let currentCardIdx = this.props.cards.indexOf(this.state.card);
+
+    //direction next true， prev false
+    let newIdx;
+    let len = this.props.cards.length - 1;    
+    let prev = currentCardIdx - 1;
+
+    if(press === undefined || press === null){
+      this.state.direction ? newIdx = currentCardIdx + 1 : prev < 0 ? newIdx = len : newIdx = prev;
+    }else{
+      press === 'right' ? newIdx = currentCardIdx + 1 : prev < 0 ? newIdx = len : newIdx = prev;
+    }
+
+    // Checks to see if last card.
+    // If props.loop=true, will start again from the first card.
+    let card = newIdx > len
+      ? this.props.loop ? this.props.cards[0] : null
+      : this.props.cards[newIdx];
+
+    card['CardPosition'] = false;
+    this.setState({
+      card: card,
+    });
+  }
+
+  componentDidMount() {
+    this._animateEntrance();
+  }
+  
+  _animateEntrance() {
+    Animated.spring(
+      this.state.enter,
+      { toValue: 1, friction: 8 }
+    ).start();
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.cards && nextProps.cards.length > 0){
+      this.setState({
+        card: nextProps.cards[0]
+      })
+    }
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => {
+
+        return gestureState.dx != 0 && gestureState.dy != 0;
+      },
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
+        this.state.pan.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event([
+        null, {dx: this.state.pan.x, dy: this.state.pan.y},
+      ]),
+
+      onPanResponderRelease: (e, {vx, vy}) => {
+
+        this.state.pan.flattenOffset();
+        var velocity;
+
+        if (vx >= 0) {
+          velocity = clamp(vx, 3, 5);
+        } else if (vx < 0) {
+          velocity = clamp(vx * -1, 3, 5) * -1;
+        }
+
+        if (Math.abs(this.state.pan.x._value) > SWIPE_THRESHOLD) {
+
+          let indexOf = this.props.cards.indexOf(this.state.card);
+          let len = this.props.cards.length - 1;
+
+          this.state.pan.x._value > 0
+            ? this.props.handleYup(this.state.card, indexOf + 1> len ? this.props.cards[0] : this.props.cards[indexOf + 1])
+            : this.props.handleNope(this.state.card, indexOf - 1 < 0 ? this.props.cards[len] : this.props.cards[indexOf - 1])
+
+          this.props.cardRemoved
+            ? this.props.cardRemoved(indexOf)
+            : null
+          this.state.pan.x._value > 0 ? this.state.direction = true : this.state.direction = false;
+
+          Animated.decay(this.state.pan, {
+            velocity: {x: velocity, y: vy},
+            deceleration: 0.98
+          }).start(this._resetState.bind(this, null))
+        } else {
+          Animated.spring(this.state.pan, {
+            toValue: {x: 0, y: 0},
+            friction: 4
+          }).start()
+        }
+      }
+    });
+  }
+
+  _resetState(str) {
+    this.state.pan.setValue({x: 0, y: 0});
+    this.state.enter.setValue(0);
+    this._goToNextCard(str);
+    this._animateEntrance();
+  }
+
+  _cardList() {
+
+    if(this.state.card === undefined) return false;
+    
+    let list = [];
+   // let rank = this.state.card.rank; this.props.cards.indexOf(this.state.card);
+    let rank = this.props.cards.indexOf(this.state.card) + 1;
+    
+    let len = this.props.cards.length - 1;
+    let cardsView = [];
+    if(this.state.direction){
+      for(let i = 0; i < 3; i++){
+        let id = i + rank, card = [];
+        if(id > len){
+          rank = 0;
+          id = rank + i
+        }
+        card = this.props.cards[id];
+        card.CardPosition = true;
+        card.CardTop = (i + 1)^3 * (i + 2) ;
+        card.key = i;
+        cardsView.unshift(this.renderCard(card, false));
+      }
+
+    } else {
+      for(let i = 4; i > 0; i--){
+        let card = [], id = 0;
+        
+        id = rank - i- 1;
+        if(id < 0){
+          id = id + len + 1;
+        }
+       
+        card = this.props.cards[id];
+        card.CardPosition = true;
+        card.CardTop = (i + 1)^3 * (i + 2) ;
+        card.key = i;
+        cardsView.push(this.renderCard(card, false));
+      }
+    }
+    return cardsView;
+  }
+  
+
+  pressNext = (str) => {
+    let dir, valueXY;
+
+    if(str == 'right') {
+      valueXY = {x: 220, y: 60};
+      dir = true;
+    }else{
+      valueXY = {x: -220, y: 60};
+      dir = false;
+    }
+
+    this.setState({
+      direction: dir,
+    });
+
+    Animated.spring(this.state.pan, {
+      toValue: valueXY,
+      bounciness: 10,
+    }).start(this._resetState.bind(this, str));
+
+  }
+
+  renderNoMoreCards() {
+    if (this.props.renderNoMoreCards)
+      return this.props.renderNoMoreCards();
+    return (
+      <Defaults.NoMoreCards />
+    )
+  }
+
+  renderCard(cardData, lastBoo) {
+    if(lastBoo) {
+      let id = cardData.subject ? cardData.subject.id : cardData.id;
+      AsyncStorage.setItem('panId', id);
+    }
+    return this.props.renderCard(cardData)
+  }
+
+  render() {
+    let { pan, enter, } = this.state;
+
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    let rotate = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: ["-30deg", "0deg", "30deg"]});
+    let opacity = pan.x.interpolate({inputRange: [-200, 0, 200], outputRange: [0.5, 1, 0.5]});
+    let scale = enter;
+
+    let animatedCardstyles = {transform: [{translateX}, {translateY}, {rotate}, {scale}], opacity};
+
+    let yupOpacity = pan.x.interpolate({inputRange: [0, 150], outputRange: [0, 1]});
+    let yupScale = pan.x.interpolate({inputRange: [0, 150], outputRange: [0.5, 1], extrapolate: 'clamp'});
+    let animatedYupStyles = {transform: [{scale: yupScale}], opacity: yupOpacity}
+
+    let nopeOpacity = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0]});
+    let nopeScale = pan.x.interpolate({inputRange: [-150, 0], outputRange: [1, 0.5], extrapolate: 'clamp'});
+    let animatedNopeStyles = {transform: [{scale: nopeScale}], opacity: nopeOpacity}
+
+        return (
+            <View style={this.props.containerStyle}>
+                {
+                  <View style={{position: 'absolute'}}>{this._cardList()}</View>
+                }
+                { this.state.card
+                    ? (
+                    <Animated.View style={[this.props.cardStyle, animatedCardstyles]} {...this._panResponder.panHandlers}>
+                        {this.renderCard(this.state.card, true)}
+                    </Animated.View>
+                )
+                  : this.renderNoMoreCards() }
+
+
+                { this.props.renderNope
+                  ? this.props.renderNope(pan, this.pressNext)
+                  : (
+                      this.props.showNope
+                      ? (
+                        <Animated.View style={[this.props.nopeStyle, this.props.showNopeChange ? animatedNopeStyles : null]}>
+                            {this.props.noView
+                                ? this.props.noView
+                                : <Text style={this.props.nopeTextStyle}>{this.props.noText ? this.props.noText : "Nope!"}</Text>
+                            }
+                        </Animated.View>
+                        )
+                      : null
+                    )
+                }
+                { this.props.renderYup
+                  ? this.props.renderYup(pan, this.pressNext)
+                  : (
+                      this.props.showYup
+                      ? (
+                        <Animated.View style={[this.props.yupStyle, this.props.showYupChange ? animatedYupStyles : null]}>
+                            {this.props.yupView
+                                ? this.props.yupView
+                                : <Text style={this.props.yupTextStyle}>{this.props.yupText ? this.props.yupText : "Yup!"}</Text>
+                            }
+                        </Animated.View>
+                      )
+                      : null
+                    )
+                }
+            </View>
+    );
+  }
+}
+ //animatedYupStyles  animatedNopeStyles
+SwipeCards.propTypes = {
+    cards: React.PropTypes.array,
+    renderCards: React.PropTypes.func,
+    loop: React.PropTypes.bool,
+    renderNoMoreCards: React.PropTypes.func,
+    showYup: React.PropTypes.bool,
+    showNope: React.PropTypes.bool,
+    handleYup: React.PropTypes.func,
+    handleNope: React.PropTypes.func,
+    yupView: React.PropTypes.element,
+    yupText: React.PropTypes.string,
+    noView: React.PropTypes.element,
+    noText: React.PropTypes.string,
+    containerStyle: View.propTypes.style,
+    cardStyle: View.propTypes.style,
+    yupStyle: View.propTypes.style,
+    yupTextStyle: Text.propTypes.style,
+    nopeStyle: View.propTypes.style,
+    nopeTextStyle: Text.propTypes.style,
+};
+
+SwipeCards.defaultProps = {
+    loop: false,
+    showYup: true,
+    showNope: true,
+    containerStyle: styles.container,
+    yupStyle: styles.yup,
+    yupTextStyle: styles.yupText,
+    nopeStyle: styles.nope,
+    nopeTextStyle: styles.nopeText,
+};
+
+export default SwipeCards
